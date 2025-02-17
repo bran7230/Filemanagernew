63e2dc718396c2378db43e8d9c5a489d740b6ddf
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-02-17 03:28:20 -0500

    Made it so you can adjust file by writting and open said file

--- src/guiFile.java
+++ src/guiFile.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import javax.swing.*;
@@ -56,8 +57,12 @@ public class guiFile extends FileManager {
         menuBar.add(fileMenu);
 
         openMenuItem.addActionListener(_->{
-            openFileFrame();
-            openFileFrame().setVisible(true);
+            try {
+                openFileFrame();
+            }
+            catch(IOException e){
+               System.out.println(e);
+            }
         });
 
         saveMenuItem.addActionListener(_->{
@@ -101,7 +106,7 @@ public class guiFile extends FileManager {
         submitButton.addActionListener(_->{
             String name = textField.getText();
             try {
-                fm.createFile(name);
+                fm.createFile(name+".txt");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -118,12 +123,55 @@ public class guiFile extends FileManager {
         return createframe;
 
     }
-    public static JFrame openFileFrame(){
+    public static JFrame openFileFrame() throws IOException {
 
         JFrame openFileframe = new JFrame("Choose a file");
+        JMenuBar menubar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem saveMenuItem = new JMenuItem("Save");
         openFileframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        fileMenu.add(saveMenuItem);
+        menubar.add(fileMenu);
+        openFileframe.setJMenuBar(menubar);
+
         openFileframe.setSize(800, 600);
         openFileframe.setLocationRelativeTo(null);
+
+        JTextField textField = new JTextField(20);
+        textField.setFont(new Font("Monospaced", Font.PLAIN, 12));
+        textField.setLayout(new FlowLayout(FlowLayout.CENTER));
+        textField.setVisible(true);
+        openFileframe.add(textField);
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setCurrentDirectory(new java.io.File("."));
+
+        if (fileChooser.showOpenDialog(openFileframe) == JFileChooser.APPROVE_OPTION){
+
+            File file = fileChooser.getSelectedFile();
+
+            try {
+                textField.setText(fm.readFile(String.valueOf(file)));
+            }
+            catch(IOException e){
+                textField.setText("Error reading file "+e.getMessage());
+            }
+        }
+
+        saveMenuItem.addActionListener(_->{
+           String content = textField.getText();
+           File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            try {
+                fm.writeFile(String.valueOf(file), content);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
+
+        openFileframe.setVisible(true);
         return openFileframe;
 
     }
@@ -147,57 +195,53 @@ public class guiFile extends FileManager {
         JTextField textField = new JTextField(20);
         JButton submitButton = new JButton("Submit");
 
-        submitButton.addActionListener(new ActionListener() {
+        submitButton.addActionListener(e -> {
+            JFrame choiceframe = new JFrame("Are You Sure?");
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFrame choiceframe = new JFrame("Are You Sure?");
+            choiceframe.setSize(600, 400);
 
-                choiceframe.setSize(600, 400);
+            choiceframe.setLocationRelativeTo(null);
+            choiceframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            choiceframe.setVisible(true);
 
-                choiceframe.setLocationRelativeTo(null);
-                choiceframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                choiceframe.setVisible(true);
+            JTextField field = new JTextField(50);
+            field.setText("Action is permanent and cannot be undone");
 
-                JTextField field = new JTextField(50);
-                field.setText("Action is permanent and cannot be undone");
+            choiceframe.setLayout(new FlowLayout());
 
-                choiceframe.setLayout(new FlowLayout());
+            JButton yesButton = new JButton("YES");
+            JButton noButton = new JButton("NO");
 
-                JButton yesButton = new JButton("YES");
-                JButton noButton = new JButton("NO");
+            choiceframe.add(field);
+            choiceframe.add(yesButton);
+            choiceframe.add(noButton);
 
-                choiceframe.add(field);
-                choiceframe.add(yesButton);
-                choiceframe.add(noButton);
+            yesButton.addActionListener(_-> {
+                String filename = textField.getText();
+                try {
+                    fm.deleteFile(filename);
 
-                yesButton.addActionListener(_-> {
-                    String filename = textField.getText();
-                    try {
-                        fm.deleteFile(filename);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
 
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+                choiceframe.dispose();
+            });
+            noButton.addActionListener(_->{
 
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                    choiceframe.dispose();
-                });
-                noButton.addActionListener(_->{
-
-                    try{
-                        Thread.sleep(2000);
-                    }
-                    catch(InterruptedException ex){
-                        throw new RuntimeException(ex);
-                    }
-                    choiceframe.dispose();
-                });
-            }
+                try{
+                    Thread.sleep(2000);
+                }
+                catch(InterruptedException ex){
+                    throw new RuntimeException(ex);
+                }
+                choiceframe.dispose();
+            });
         });
 
         container.add(new JLabel("Enter a file name: "));
